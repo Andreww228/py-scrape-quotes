c5709ed2eaa7ee408bae92703747fc8d49732145
AndrewwSS <clash2clans1one@gmail.com>
2024-08-28 11:31:15 +0300

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,14 @@
+import asyncio
+import dataclasses
+from dataclasses import astuple
 from dataclasses import dataclass
+import csv
+
+import httpx
+from bs4 import BeautifulSoup
+from bs4 import Tag
+
+ROOT_URL = 'https://quotes.toscrape.com/'
 
 
 @dataclass
@@ -8,8 +18,50 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [fild.name for fild in dataclasses.fields(Quote)]
+
+
+def get_page_soup(page: int, client: httpx.Client) -> BeautifulSoup:
+    response = client.get(f"{ROOT_URL}page/{page}/")
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+
+
+def parse_quote(quote_soup: Tag) -> Quote:
+    return Quote(
+        text=quote_soup.select_one("span.text").text,
+        author=quote_soup.select_one("span > small.author").text,
+        tags=[tag.text for tag in quote_soup.select(".tags .tag")],
+    )
+
+
+def parse_quotes_page(
+    page_soup: BeautifulSoup,
+) -> list[Quote]:
+    quotes = page_soup.select(".quote")
+    return [parse_quote(quote_soup) for quote_soup in quotes]
+
+
+def write_quotes_to_csv(quotes: list[Quote], file: str) -> None:
+    with open(file, "w", encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(QUOTE_FIELDS)
+        for quote in quotes:
+            writer.writerow([quote.text, quote.author, ", ".join(quote.tags)])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    page_number = 1
+    result = []
+    with httpx.Client() as client:
+        while True:
+            page_soup = get_page_soup(page_number, client)
+            result.extend(parse_quotes_page(page_soup))
+            has_next = page_soup.select_one("ul.pager > li.next")
+            if not has_next:
+                break
+            page_number += 1
+    write_quotes_to_csv(result, output_csv_path)
 
 
 if __name__ == "__main__":
